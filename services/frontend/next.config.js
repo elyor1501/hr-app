cba8529619cdfcf59abef10872937387608ab652
sauravvaspp <saurav.kubade@vaspp.com>
2026-03-25 19:18:22 +0530

    exposed port fixed

--- services/frontend/next.config.js
+++ services/frontend/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://backend:8000/api/:path*',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
