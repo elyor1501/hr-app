f6936588cfcd81d771f18316fc0f33b2b9dd89d4
akshaypo3 <akshay@vaspp.com>
2026-03-27 16:48:32 +0530

    Update next.config.js

--- services/frontend/next.config.js
+++ services/frontend/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
-  },
 }
 
 module.exports = nextConfig
