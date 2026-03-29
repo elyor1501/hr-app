ee681cbc77efdeee824c4f6e9c0f9422c9a31e4e
KruthikaPrasad16 <kruthika.prasad@vaspp.com>
2026-03-29 12:13:30 +0530

    Display msg while extracting data from resume

--- services/frontend/components/candidate/CandidateTable.tsx
+++ services/frontend/components/candidate/CandidateTable.tsx
@@ -1,15 +1,19 @@
 import { getCandidates } from "@/lib/candidates/data";
 import { DataTable } from "@/components/table/data-table";
 import { columns_candidate_list } from "@/components/candidate/CandidateListTableColumn";
+import CandidateProcessingTracker from "./CandidateProcessingTracker";
 
 export default async function CandidatesTable() {
   const data = await getCandidates();
   return (
-    <DataTable
-      columns={columns_candidate_list}
-      data={data}
-      filter={"first_name"}
-      sort={""}
-    />
+    <>
+      <CandidateProcessingTracker candidates={data} />
+      <DataTable
+        columns={columns_candidate_list}
+        data={data}
+        filter={"first_name"}
+        sort={""}
+      />
+    </>
   );
 }
